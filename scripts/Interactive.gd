5f02d86afca160d48e750ee3272fe7fd4f540785
Osadchuk Maxim <red_fyru@ukr.net>
2024-05-19 23:13:57 +0300

    Поднятие предмета

--- scripts/Interactive.gd
+++ scripts/Interactive.gd
@@ -7,10 +7,9 @@ var target = null
 func _physics_process(delta):
 	if target:
 		global_transform = global_transform.interpolate_with(target.global_transform, delta * 10)
-		print('phisics')
 func take(_target):
-	target = target
-	print("target")
+	target = _target
+	
 	
 func drop():
 	pass
